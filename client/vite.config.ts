9345ba4fa02a63617a1762b0fb18d76dd8f8ba60
Umut <akkaya.umut094@gmail.com>
2025-03-11 23:43:41 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     plugins: [react(), tailwindcss(),],
     base: '/',
     build: {
-        outDir: 'client/dist', // Vercel'in algılaması için dist olarak ayarla
+        outDir: 'dist', // Vercel'in algılaması için dist olarak ayarla
     },
 });
